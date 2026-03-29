ffe2dc69d0656b59d9a23100df4d8533c61648be
Yamamoto Runa <k025g0073@g.neec.ac.jp>
2026-03-30 00:48:24 +0900

    Revert "a"
    This reverts commit 7dea217313293f2e418f9a404ac197f8dc8121e4.

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -6,11 +6,10 @@
 #include <cassert>
 #include <numbers>
 using namespace KamataEngine;
-void Enemy::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& position,GameScene* gameScene) {
+void Enemy::Initialize(KamataEngine::Model* model, KamataEngine::Camera* camera, const KamataEngine::Vector3& position) {
 	assert(model);
 	textureHandle_ = TextureManager::Load("uvChecker.png");
 	model_ = model;
-	gameScene_ = gameScene;
 	worldTransform_.Initialize();
 	worldTransform_.translation_ = position;
 	worldTransform_.rotation_.y = std::numbers::pi_v<float> * 3.0f / 2.0f;
